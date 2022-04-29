55ed1bee675277c7d89b34ed521cb272b54f122d
rwietter <mauriciobw17@gmail.com>
2022-04-28 23:32:40 -0300

    chore: adjust icon sizes

--- data/social-links.tsx
+++ data/social-links.tsx
@@ -2,15 +2,15 @@ import { AiOutlineTwitter, AiFillLinkedin, AiFillGithub } from 'react-icons/ai';
 
 export const SOCIAL_LINKS = [
   {
-    icon: <AiOutlineTwitter size={20} />,
+    icon: <AiOutlineTwitter size={23} />,
     url: 'https://twitter.com/rwietter',
   },
   {
-    icon: <AiFillLinkedin size={20} />,
+    icon: <AiFillLinkedin size={23} />,
     url: 'https://www.linkedin.com/in/euiciowr/',
   },
   {
-    icon: <AiFillGithub size={20} />,
+    icon: <AiFillGithub size={23} />,
     url: 'https://github.com/rwietter',
   },
 ];
